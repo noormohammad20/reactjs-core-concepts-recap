d325d8977c6ea6bc08c77fc8815a81123ef5b090
noormohammad20 <mdsaim204@gmail.com>
2022-03-18 01:40:23 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { useEffect, useState } from 'react'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LoadPosts></LoadPosts>
+      <District name="noakhali" special="vibag"></District>
+      <District name="bramonbaria" special="jodda"></District>
+      <District name="kumilla" special="moyna and moti"></District>
+
     </div>
-  );
+  )
 }
 
-export default App;
+function LoadPosts() {
+  const [posts, setPosts] = useState([])
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then(res => res.json())
+      .then(data => setPosts(data))
+  }, [])
+  return (
+    <div>
+      <h1>Posts:{posts.length} </h1>
+      {
+        posts.map(post => <Post title={post.title} body={post.body}></Post>)
+      }
+    </div>
+  )
+}
+const Post = (props) => {
+  return (
+    <div style={{ backgroundColor: 'lightgray', margin: '20px', border: '5px solid salmon' }}>
+      <h2>{props.title}</h2>
+      <p>{props.body}
+      </p>
+    </div>
+  )
+}
+
+const districtStyle = {
+  backgroundColor: 'lightblue',
+  padding: '20px',
+  margin: '20px',
+  borderRadius: '20px'
+}
+
+function District(props) {
+  const [power, setPower] = useState(1)
+  const boostPower = () => {
+    setPower(power * 2)
+  }
+  return (
+    <div style={districtStyle}>
+      <h2>name:{props.name} </h2>
+      <p>specialty: {props.bivag}</p>
+      <h4>power:{power} </h4>
+      <button onClick={boostPower}>boost the power </button>
+    </div>
+  )
+}
+
+export default App
